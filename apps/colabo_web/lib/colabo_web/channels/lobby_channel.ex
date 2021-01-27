aba16133c662732d486957e5ae9b3e7b593c225b
J <j@localhost.com>
2021-01-27 10:11:16 +0100

    Quitar IO.inspect de depuración

--- apps/colabo_web/lib/colabo_web/channels/lobby_channel.ex
+++ apps/colabo_web/lib/colabo_web/channels/lobby_channel.ex
@@ -14,16 +14,18 @@ defmodule ColaboWeb.LobbyChannel do
 
 
   def handle_in("new_patch", %{"patch" => patch}, socket) do
-    IO.puts("PatchStore.state es #{inspect(PatchStore.state())}")
+    # IO.puts("PatchStore.state es #{inspect(PatchStore.state())}")
+    IO.puts("new_patch")
+    IO.inspect(patch)
     PatchStore.add(patch)
     broadcast_from!(socket, "new_patch", %{peer_data: socket.assigns.peer_data, new_patch: patch})
-    IO.inspect(patch)
     {:reply, :ok, socket}
   end
 
   def handle_in("selection_range", %{"range" => range}, socket) do
+    IO.puts("selection_range")
     IO.inspect(range)
-    IO.inspect(socket)
+    # IO.inspect(socket)
     broadcast_from!(socket, "selection_range", %{peer_data: socket.assigns.peer_data, range: range})
     {:reply, :ok, socket}
   end
